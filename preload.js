97da3d8aa03049a0e43715c084a748ad021684a3
末心 <1044631097@qq.com>
2023-02-24 17:45:05 +0800

    快捷启动青少年CTF

---
+++ preload.js
@@ -0,0 +1,16 @@
+window.exports = {
+    "hello": { // 注意：键对应的是 plugin.json 中的 features.code
+       mode: "none",  // 用于无需 UI 显示，执行一些简单的代码
+       args: {
+          // 进入插件应用时调用
+          enter: (action) => {
+             // action = { code, type, payload }
+             window.utools.hideMainWindow()
+             // do some thing
+
+             utools.shellOpenExternal('https://www.qsnctf.com')
+             window.utools.outPlugin()
+          }  
+       } 
+    }
+ }
